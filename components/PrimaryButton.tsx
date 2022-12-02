ba588191d5b0a64a0a9e4fd2401539e4a63022ac
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-12-02 17:03:28 -0500

    -

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,7 +1,53 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Pressable,
+  GestureResponderEvent,
+  StyleSheet,
+  Text,
+} from 'react-native';
 
-function PrimaryButton({ children }: { children?: JSX.Element }) {
-  return <View>{children}</View>;
+function PrimaryButton({ children }: { children?: string }) {
+  const thing = (e: GestureResponderEvent): void => {
+    console.log(e.currentTarget);
+  };
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={thing}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 9,
+    paddingHorizontal: 16,
+    //Andriod Styles
+    elevation: 4,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
