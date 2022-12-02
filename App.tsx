7496ac2939f1e795b9ba43ae606b3d407f0ecc5c
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-12-02 17:03:24 -0500

    -

--- App.tsx
+++ App.tsx
@@ -2,11 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return (
-    <View>
-      <StartGameScreen />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({});
