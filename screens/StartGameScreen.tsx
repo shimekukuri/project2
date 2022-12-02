9ed917246beda123b09cf82524ce4fe541635807
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-12-02 17:03:31 -0500

    -

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,17 +1,64 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-    <View>
-      <PrimaryButton>
-        <Text>test</Text>
-      </PrimaryButton>
-      <PrimaryButton>
-        <Text>Test2</Text>
-      </PrimaryButton>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType={'number-pad'}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    alignItems: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: '#4e0329',
+    //android styles
+    elevation: 4,
+    //IOS styles
+    shadowColor: 'black',
+    shadowRadius: 6,
+    shadowOffset: { width: 0, height: 2 },
+    textShadowRadius: 8,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+  },
+  button: {
+    flex: 1,
+  },
+});
 
 export default StartGameScreen;
